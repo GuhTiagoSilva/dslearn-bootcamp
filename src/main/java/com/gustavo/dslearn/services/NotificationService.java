56b8a8d3aa0a686142d08fb2a69ac0e803475949
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-04-02 20:16:13 -0300

    Saving notification after deliver (Without observer pattern)

--- src/main/java/com/gustavo/dslearn/services/NotificationService.java
+++ src/main/java/com/gustavo/dslearn/services/NotificationService.java
@@ -1,5 +1,7 @@
 package com.gustavo.dslearn.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gustavo.dslearn.dto.NotificationDTO;
+import com.gustavo.dslearn.entities.Deliver;
+import com.gustavo.dslearn.entities.Notification;
 import com.gustavo.dslearn.entities.User;
 import com.gustavo.dslearn.repositories.NotificationRepository;
 
@@ -23,6 +27,23 @@ public class NotificationService {
 	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, PageRequest pageRequest){
 		User user = authService.authenticated();
 		return repository.find(user, unreadOnly, pageRequest).map( notification -> new NotificationDTO(notification));
+	}
+	
+	@Transactional
+	public void saveDeliverNotification(Deliver deliver) {
+		
+		Long sectionId = deliver.getLesson().getSection().getId();
+		Long resourceId = deliver.getLesson().getSection().getResource().getId();
+		Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+		
+		String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+		String text = deliver.getFeedback();
+		Instant moment = Instant.now();
+		User student = deliver.getEnrollment().getStudent();
+		
+		
+		Notification notification = new Notification(null, text, moment, false, route, student);
+		repository.save(notification);
 	}
 	
 }
